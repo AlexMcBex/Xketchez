ff77e5e799b5fbc716a553cb2cf769061537519f
AlexMcBex <alessiobelli94@gmail.com>
2023-03-08 11:33:48 -0500

    made Art model and implemented it in views index

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Art(models.Model):
+    title = models.CharField(max_length=50)
+    author = models.CharField(max_length=50)
+    type = models.CharField(max_length=50)
+    method = models.CharField(max_length=200)
+    comment = models.CharField(max_length=300)
+    likes = models.IntegerField
+    description = models.CharField(max_length=300)
+
+    def __str__(self):
+        return f'{self.title} by {self.author}'
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
+from .models import Art
 
-arts = [
-    {'title' : "Ramona Flowers' portrait", 'author': 'Scott Pilgrim', 'type': 'drawing', 'method': 'marker on A3 paper', 'comment': 'Have you seen a woman at a party with this hair?', 'likes': 4, 'file': 'ramona-flowers.jpg', 'description': 'A portrait of Ramona Flowers', 'year': 2008},
-    {'title' : "Human Perfection", 'author': 'Leonardo da Vinci', 'type': 'drawing', 'method': 'pencil on scroll', 'comment': 'Modestamente, ho creato questo in 10 minuti.', 'likes': 10000000000, 'file': 'leonardo-da-vinci.webp', 'description': 'the famouse sketch by Leonardo da Vinci', 'year': 1412},
-    {'title' : "Heisenberg", 'author': 'DEA', 'type': 'sketch', 'method': 'pen on paper', 'comment': 'Do you know this man?', 'likes': 2301, 'file': 'heisenberg.jpg', 'description': 'a sketch of heisenberg', 'year': 2010}
-]
+# arts = [
+#     {'title' : "Ramona Flowers' portrait", 'author': 'Scott Pilgrim', 'type': 'drawing', 'method': 'marker on A3 paper', 'comment': 'Have you seen a woman at a party with this hair?', 'likes': 4, 'file': 'ramona-flowers.jpg', 'description': 'A portrait of Ramona Flowers', 'year': 2008},
+#     {'title' : "Human Perfection", 'author': 'Leonardo da Vinci', 'type': 'drawing', 'method': 'pencil on scroll', 'comment': 'Modestamente, ho creato questo in 10 minuti.', 'likes': 10000000000, 'file': 'leonardo-da-vinci.webp', 'description': 'the famouse sketch by Leonardo da Vinci', 'year': 1412},
+#     {'title' : "Heisenberg", 'author': 'DEA', 'type': 'sketch', 'method': 'pen on paper', 'comment': 'Do you know this man?', 'likes': 2301, 'file': 'heisenberg.jpg', 'description': 'a sketch of heisenberg', 'year': 2010}
+# ]
 
 # Create your views here.
 def home(req):
@@ -14,4 +15,5 @@ def about(req):
     return render(req, 'about.html')
 
 def arts_index(req):
+    arts= Art.Objects.all()
     return render(req, 'arts/index.html', { 'arts' : arts })
