cbeb1d7cbda0daeedf0ae14b3659de4c9924e947
AlexMcBex <alessiobelli94@gmail.com>
2023-03-09 16:38:44 -0500

    added comment model

--- main_app/models.py
+++ main_app/models.py
@@ -12,7 +12,8 @@ class Art(models.Model):
     likes = models.IntegerField
     description = models.CharField(max_length=300)
     file = models.CharField(max_length=200, default="smile.jpg")
-    # created = models.DateTimeField(auto_now_add=True)
+    # date = models.DateTimeField(auto_now_add=True)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.title} by {self.author}'
@@ -26,3 +27,11 @@ class Photo(models.Model):
 
     def __str__(self):
         return f"Photo for art_id: {self.art_id} @{self.url}"
+    
+    class Comment(models.Model):
+        date = models.DateTimeField(auto_now_add=True)
+        text = models.CharField(max_length=500)
+        user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+        def __str__ (self):
+            return f'{self.user.username} commented: \n{self.text}'
